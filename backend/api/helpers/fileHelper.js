6d6a538c708a00b6ca49d82fbfa434cba86ecaaf
Nicolas GIGOU <nicolas.gigou@gmail.com>
2018-01-13 00:34:21 +0100

    Handle videos in fileHelper

--- backend/api/helpers/fileHelper.js
+++ backend/api/helpers/fileHelper.js
@@ -9,12 +9,20 @@ const IMAGES_EXTENSIONS = [
     'JPEG'
 ]
 
+const VIDEOS_EXTENSIONS = [
+    'MP4'
+]
+
 module.exports = {
 
+    /**
+     * Parse a filename to get its extension and its type (image, video or whatever).
+     * @param {String} filename - The name of the file, with its extension if existing.
+     * @returns {Object} The extension and the type of media if handled or an empty object if not.
+     */
     getFileExtension(filename) {
         if (!filename) {
             LOG.warn(`Missing filename param to get its extension.`)
-
             return {}
         }
         LOG.info('------------------------ New file ------------------------')
@@ -23,17 +31,19 @@ module.exports = {
         let capitalized = extension.toUpperCase()
         LOG.info(`Caught file extension: ${extension} / ${capitalized}`)
         if (IMAGES_EXTENSIONS.includes(capitalized)) {
-
+            LOG.info('The file is an image.')
             return {
                 extension: capitalized,
                 type: 'image'
             }
+        } else if (VIDEOS_EXTENSIONS.includes(capitalized)) {
+            LOG.info('The file is a video.')
+            return {
+                extension: capitalized,
+                type: 'video'
+            }
         }
-        LOG.info('The file is a video. Or something else. But not an image.')
-
-        return {
-            extension: capitalized,
-            type: 'video'
-        }
+        LOG.warn('The media is a folder. Or something else. But not an image or a video.')
+        return {}
     }
 }
