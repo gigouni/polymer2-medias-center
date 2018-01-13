04844ff8d1afb941d608cca925e0f90d0546257c
Nicolas GIGOU <nicolas.gigou@gmail.com>
2018-01-13 01:04:19 +0100

    Add tests gabarit

---
+++ backend/test/api/helpers/fileHelper.test.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert
+
+/* global describe, it */
+
+describe('FileHelper', () => {
+
+    describe('#getFileExtension', () => {
+
+        it('needs to be implemented', () => {
+            assert.fail(0, 1, 'Needs to be implemented')
+        })
+    })
+
+})
